1ac57a59c2013d1fb25c2356287d16228990d64f
Ruchir Dixit <ruchirtd96@gmail.com>
2020-08-05 20:18:25 +0530

    Daily Wage

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -4,10 +4,13 @@ public class EmpWageComputationProg
 	{
 		//System.out.println("Welcome to Employee Wage Computation Program");
 		final int IS_PRESENT=1;
+		final int EMP_RATE_PER_HR=20;
+		int empHrs=0;
+		int dailyWage=0;
 		double empCheck=Math.floor(Math.random()*10)%2;
 		if (empCheck == IS_PRESENT)
-			System.out.println("Emp is present");
-		else
-			System.out.println("Emp is absent");
+			empHrs=8;
+		dailyWage=empHrs*EMP_RATE_PER_HR;
+		System.out.println("Daily wage of employee is:"+dailyWage);
 	}
 }
