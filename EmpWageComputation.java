5cede48c32b5f5c76600b7b87c2b861a1d8de98b
Ruchir Dixit <ruchirtd96@gmail.com>
2020-08-05 20:14:54 +0530

    Attendance

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,7 +1,13 @@
-public class EmpWageComputation
+public class EmpWageComputationProg
 {
 	public static void main(String args[])
 	{
-		System.out.println("Welcome to Emp Wage program on master branch!");
+		//System.out.println("Welcome to Employee Wage Computation Program");
+		final int IS_PRESENT=1;
+		double empCheck=Math.floor(Math.random()*10)%2;
+		if (empCheck == IS_PRESENT)
+			System.out.println("Emp is present");
+		else
+			System.out.println("Emp is absent");
 	}
 }
