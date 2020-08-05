315bd6c67a96678f3b84e9e4e9f3455989ae6613
Ruchir Dixit <ruchirtd96@gmail.com>
2020-08-05 21:42:08 +0530

    For loop

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,4 +1,4 @@
-public class EmpWageComputation
+public class EmpWageComputationProg
 {
 	public static void main(String args[])
 	{
@@ -6,22 +6,27 @@ public class EmpWageComputation
 		final int IS_FULL_TIME=1;
 		final int IS_PART_TIME=2;
 		final int EMP_RATE_PER_HR=20;
+		final int MAX_WORKINGS_DAYS=20;
 		int empHrs=0;
 		int dailyWage=0;
-		double empCheck=Math.floor(Math.random()*10)%3;
-		switch((int)empCheck)
+		for (int day=1; day<=MAX_WORKINGS_DAYS; day++)
 		{
-			case IS_FULL_TIME:
-						empHrs=8;
-						break;
-			case IS_PART_TIME:
-						empHrs=4;
-						break;
-			default:
-						empHrs=0;
-						break;
+			double empCheck=Math.floor(Math.random()*10)%3;
+			switch((int)empCheck)
+      	{
+         	case IS_FULL_TIME:
+                  empHrs=8;
+                  break;
+         	case IS_PART_TIME:
+                  empHrs=4;
+                  break;
+         	default:
+                  empHrs=0;
+                  break;
+      	}
+		dailyWage+=empHrs;
 		}
-		dailyWage=empHrs*EMP_RATE_PER_HR;
-		System.out.println("Daily wage of employee is:"+dailyWage);
+		System.out.println("Wage of Emp after "+MAX_WORKINGS_DAYS+" days is: "+dailyWage*EMP_RATE_PER_HR);
 	}
 }
+
